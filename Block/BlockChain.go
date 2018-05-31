9372fe24cd43c510a2598eec7f15f4479f591009
dom <domdou@tsinkia.com>
2018-05-31 18:09:42 +0800

    签名

--- Block/BlockChain.go
+++ Block/BlockChain.go
@@ -6,6 +6,9 @@ import (
 	"encoding/hex"
 	"os"
 	"fmt"
+	"errors"
+	"bytes"
+	"crypto/ecdsa"
 )
 
 //数据库文件位置
@@ -218,9 +221,16 @@ func (bc *BlockChain) FindUnspentTransactions(pubKeyHash []byte) []Transaction {
 
 	return unspentTXs
 }
-
+//将新的交易保存到数据库
 func (bc *BlockChain) MineBlock(transactions []*Transaction) {
 	var lastHash []byte
+
+	for _,tx := range transactions{
+		if bc.VerifyTransaction(tx) != true {
+			log.Panic("ERROR:Invalid transaction")
+		}
+	}
+
 	err := bc.DB.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
 		lastHash = b.Get([]byte("l"))
@@ -245,6 +255,7 @@ func (bc *BlockChain) MineBlock(transactions []*Transaction) {
 	})
 }
 
+//查找到未花费的输出（余额）
 func (bc *BlockChain) FindUTXO(address string) []TXOutput {
 	pubKeyHash := Base58Decode([]byte(address))
 	pubKeyHash = pubKeyHash[1:len(pubKeyHash)-4]
@@ -260,12 +271,15 @@ func (bc *BlockChain) FindUTXO(address string) []TXOutput {
 	return UTXOs
 }
 
+//寻找所有能花费的输出  将输出累加起来达到需要交易的值时将输出的集合返回
 func (bc *BlockChain) FindSpendableOutputs(address string, amount int) (int, map[string][]int) {
 	unspentOutputs := make(map[string][]int)
 	pubKeyHash := Base58Decode([]byte(address))
 	pubKeyHash = pubKeyHash[1:len(pubKeyHash)-4]
+	//找到所有能花费的输出
 	unspentTXs := bc.FindUnspentTransactions(pubKeyHash)
 	accumulated := 0
+	//不断循环累加 直到输出的值的和 大于需要付出的值
 Work:
 	for _, tx := range unspentTXs {
 		txID := hex.EncodeToString(tx.ID)
@@ -280,4 +294,47 @@ Work:
 		}
 	}
 	return accumulated, unspentOutputs
+}
+
+//通过交易ID寻找对应的交易
+func (bc *BlockChain) FindTransaction(ID []byte) (Transaction,error){
+	bci := bc.Iterator()
+	for{
+		block := bci.Next()
+
+		for _ ,tx := range block.Transactions{
+			if bytes.Compare(tx.ID,ID) == 0{
+				return *tx,nil
+			}
+		}
+
+		if len(block.PrevHash) == 0{
+			break
+		}
+	}
+	return Transaction{},errors.New("Transaction is not found")
+}
+
+func (bc *BlockChain) SignTransaction(tx *Transaction,privKey ecdsa.PrivateKey){
+	prevTXs := make(map[string]Transaction)
+	for _,vin := range tx.Vin{
+		prevTX , err := bc.FindTransaction(vin.Txid)
+		if err != nil {
+			log.Panic(err)
+		}
+		prevTXs[hex.EncodeToString(prevTX.ID)]=prevTX
+	}
+	tx.Sign(privKey,prevTXs)
+}
+
+func (bc *BlockChain) VerifyTransaction(tx *Transaction) bool{
+	prevTXs := make(map[string]Transaction)
+	for _,vin := range tx.Vin{
+		prevTX , err := bc.FindTransaction(vin.Txid)
+		if err != nil {
+			log.Panic(err)
+		}
+		prevTXs[hex.EncodeToString(prevTX.ID)]=prevTX
+	}
+	return tx.Verify(prevTXs)
 }
--- Block/Transaction.go
+++ Block/Transaction.go
@@ -1,33 +1,38 @@
 package Block
 
 import (
+	"math/big"
 	"fmt"
 	"log"
 	"encoding/hex"
 	"bytes"
 	"encoding/gob"
 	"crypto/sha256"
+	"crypto/rand"
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"os"
 )
 
 const subsidy = 10
 
+//交易
 type Transaction struct {
-	ID   []byte
-	Vin  []TXInput
-	Vout []TXOutput
+	ID   []byte  //交易的唯一标识
+	Vin  []TXInput	//交易所包含的输入 可以多条
+	Vout []TXOutput  //交易所包含的输出 可以多条
 }
-
+//输入  输入及之前的交易的输出
 type TXInput struct {
-	Txid      []byte
-	Vout      int
-	Signature []byte
-	PubKey    []byte
+	Txid      []byte	//之前的ID
+	Vout      int	//引用的输出在之前交易中的索引
+	Signature []byte //
+	PubKey    []byte  //
 }
-
+//输出
 type TXOutput struct {
-	Value        int
-	PubKeyHash []byte
+	Value        int //金额
+	PubKeyHash []byte  //验证
 }
 
 func (in *TXInput) UsesKey(pubHashKey []byte) bool {
@@ -110,6 +115,89 @@ func NewUTXOTransaction(wallet *Wallet,from, to string, amount int, bc *BlockCha
 	}
 	tx := Transaction{nil, inputs, outputs}
 	tx.SetID()
-
+	bc.SignTransaction(&tx,wallet.PrivateKey)
 	return &tx
+}
+
+
+func (tx *Transaction) Sign(privKey ecdsa.PrivateKey,prevTXs map[string]Transaction){
+	if tx.IsCoinbase(){
+		return 
+	}
+
+	for _,vin := range tx.Vin{
+		if prevTXs[hex.EncodeToString(vin.Txid)].ID == nil {
+			log.Panic("ERROR: Previous transaction is not correct")
+		}
+	}
+
+	txCopy := tx.TrimmedCopy()
+	for inID,vin := range txCopy.Vin {
+		prevTx := prevTXs[hex.EncodeToString(vin.Txid)]
+		txCopy.Vin[inID].Signature = nil
+		txCopy.Vin[inID].PubKey = prevTx.Vout[vin.Vout].PubKeyHash
+		dataToSing := fmt.Sprintf("%x\n",txCopy)
+		r,s,err := ecdsa.Sign(rand.Reader,&privKey,[]byte(dataToSing))
+		if err != nil {
+			log.Panic(err)
+		}
+		signature := append(r.Bytes(),s.Bytes()...)
+		tx.Vin[inID].Signature = signature
+		tx.Vin[inID].PubKey = nil
+	}
+
+}
+
+func (tx *Transaction) TrimmedCopy() Transaction{
+	var inputs []TXInput
+	var outputs []TXOutput
+
+	for _ , in := range tx.Vin{
+		inputs = append(inputs,TXInput{in.Txid,in.Vout,nil,nil})
+	}
+
+	for _,out := range tx.Vout{
+		outputs = append(outputs,TXOutput{out.Value,out.PubKeyHash})
+	}
+
+	txCopy := Transaction{tx.ID,inputs,outputs}
+	return txCopy
+}
+
+func (tx *Transaction) Verify(prevTXs map[string]Transaction) bool{
+	if tx.IsCoinbase() {
+		return true
+	}
+
+	for _, vin := range tx.Vin {
+		if prevTXs[hex.EncodeToString(vin.Txid)].ID == nil {
+			log.Panic("ERROR: Previous transaction is not correct")
+		}
+	}
+
+	txCopy := tx.TrimmedCopy()
+	curve := elliptic.P256()
+	
+	for inID,vin := range tx.Vin {
+		prevTx := prevTXs[hex.EncodeToString(vin.Txid)]
+		txCopy.Vin[inID].Signature = nil
+		txCopy.Vin[inID].PubKey = prevTx.Vout[vin.Vout].PubKeyHash
+		r := big.Int{}
+		s := big.Int{}
+		sigLen := len(vin.Signature)
+		r.SetBytes(vin.Signature[:sigLen/2])
+		s.SetBytes(vin.Signature[sigLen/2:])
+		x := big.Int{}
+		y := big.Int{}
+		keyLen := len(vin.PubKey)
+		x.SetBytes(vin.PubKey[:keyLen/2])
+		y.SetBytes(vin.PubKey[keyLen/2:])
+		dataToVerify := fmt.Sprintf("%x\n",txCopy)
+		rawPubKey := ecdsa.PublicKey{Curve:curve,X:&x,Y:&y}
+		if ecdsa.Verify(&rawPubKey,[]byte(dataToVerify),&r,&s) == false{
+			return false
+		}
+		txCopy.Vin[inID].PubKey = nil
+	}
+	return true
 }
